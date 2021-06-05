9444b840d1b0ee5e7c3b800d6e5ee31f12e5ab83
MdSaddamKazmi <54942497+MdSaddamKazmi@users.noreply.github.com>
2021-06-05 22:57:21 +0530

    Update formatter.js

--- formatter.js
+++ formatter.js
@@ -24,16 +24,39 @@
   			},
 
   			// eslint-disable-next-line camelcase
+  			available_capacity_icon: function (sValue) {
+
+  				if (sValue === 0) {
+
+  					return "sap-icon://thumb-down";
+  				} else {
+
+  					return "sap-icon://thumb-up";
+  				}
+  			},
+
+  			vaccine_image: function (sValue) {
+
+  				if (sValue === "COVISHIELD") {
+
+  					return "../images/covishield.jpg";
+  				} else if (sValue === "COVAXIN") {
+
+  					return "../images/covaxin.jpg";
+  				}
+  			},
+
+  			// eslint-disable-next-line camelcase
   			link_enabled: function (sValue) {
 
   				// var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
   				var result;
   				if (sValue > 0) {
-  				
+
   					result = Boolean("true");
   				} else {
-  				
-  				result = Boolean(0);
+
+  					result = Boolean(0);
   				}
   				sValue = result;
   				return sValue;
